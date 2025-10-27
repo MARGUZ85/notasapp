57e3ce1a4e811a8072c678323dafdc060c4836b9
MANUEL RICO <rico31858@gmail.com>
2025-10-26 19:39:24 -0600

    Terminado

--- app/src/main/java/com/example/notasapp/EditNoteScreen.kt
+++ app/src/main/java/com/example/notasapp/EditNoteScreen.kt
@@ -1,9 +1,13 @@
 package com.example.notasapp
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -23,7 +27,21 @@ fun EditNoteScreen(
 
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text("Editar Nota") })
+            TopAppBar(
+                title = {
+                    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                        Text("Editar Nota", color = Color.White)
+                    }
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Volver", tint = Color.White)
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFF4CAF50) // Verde
+                )
+            )
         }
     ) { padding ->
         Column(
